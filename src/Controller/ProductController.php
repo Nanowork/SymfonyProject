c285bc10188c4cc0a05fae10284ce99934ef7c6f
Nanowork <nano_work9806@abv.bg>
2019-08-27 14:20:17 +0300

    Implemented product logic

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Entity\User;
+use App\Form\CreateType;
+use App\Form\EditProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create_product")
+     * @param Request $request
+     * @return RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function create(Request $request)
+    {
+        $user = $this->getUser();
+        $product = new Product();
+        $product->setUser($user);
+        $product->setCreatedAt(new \DateTime());
+        $product->setUpdatedAt(new \DateTime());
+        $form = $this->createForm(
+            CreateType::class,
+            $product
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('product/createProduct.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+
+    /**
+     * @Route("/editproduct/{id}", name="edit_product")
+     * @param Product $product
+     * @param Request $request
+     * @return RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function editProduct(Product $product, Request $request)
+    {
+        $product->setCreatedAt(new \DateTime());
+        $product->setUpdatedAt(new \DateTime());
+
+        $form = $this->createForm(
+            EditProductType::class,
+            $product
+        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $product = $form->getData();
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('product/editProduct.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    /**
+     * @Route("deleteproduct/{id}", name="delete_product")
+     * @param $id
+     * @return Response
+     */
+    public function deleteProduct($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No record found for product with id :' . $id
+            );
+        }
+
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('index');
+
+    }
+}
---
+++ src/Form/CreateType.php
@@ -0,0 +1,84 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Category;
+
+class CreateType extends AbstractType
+{
+
+    private $em;
+
+    /**
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+      $builder->add('name', TextType::class)
+          ->add('image', TextType::class)
+          ->add('price', NumberType::class);
+         $builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'));
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
+  }
+
+    protected function addElements(FormInterface $form, Category $category = null)
+    {
+        // 4. Add the province element
+        $form->add('category', EntityType::class, array(
+            'required' => true,
+            'data' => $category,
+            'placeholder' => 'Select a Category..',
+            'class' => 'App\Entity\Category'
+        ))
+            ->add('Create', SubmitType::class);
+    }
+
+    function onPreSubmit(FormEvent $event) {
+        $form = $event->getForm();
+        $data = $event->getData();
+
+        $category = $this->em->getRepository('App\Entity\Category')->find($data['category']);
+
+        $this->addElements($form, $category);
+    }
+
+    function onPreSetData(FormEvent $event) {
+        $product = $event->getData();
+        $form = $event->getForm();
+
+        // When you create a new person, the City is always empty
+        $category = $product->getCategory() ? $product->getCategory() : null;
+
+        $this->addElements($form, $category);
+    }
+
+
+        public function configureOptions(OptionsResolver $resolver)
+  {
+      $resolver->setDefaults([
+          'data_class' => Product::class
+      ]);
+  }
+
+}
